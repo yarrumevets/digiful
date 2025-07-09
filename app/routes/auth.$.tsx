d9e1d90da4cdf25452a6394f7945084f203b55cc
Steve <steve@yarrumevets.com>
2025-07-09 11:15:51 -0700

    Added simple console log to auth flow.

--- app/routes/auth.$.tsx
+++ app/routes/auth.$.tsx
@@ -1,8 +1,9 @@
+// app/routes/auth/$.tsx
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  await authenticate.admin(request);
-
+  const { session } = await authenticate.admin(request);
+  console.log("App installed on: ", session.shop);
   return null;
 };
