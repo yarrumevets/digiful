4d088e47b527c68280ef046f8f9e9bed147b1bd9
Steve <steve@yarrumevets.com>
2025-07-09 11:09:49 -0700

    Added a utilities file to keep misc functions.

--- app/utils/s3.ts
+++ app/utils/s3.ts
@@ -4,7 +4,12 @@ import {
   PutObjectCommand,
   DeleteObjectCommand,
 } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+// import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const S3_BUCKET = "" + process.env.S3_BUCKET;
+const S3_ACCESS_KEY = "" + process.env.S3_ACCESS_KEY;
+const S3_SECRET_ACCESS_KEY = "" + process.env.S3_SECRET_ACCESS_KEY;
+const S3_REGION = "" + process.env.S3_REGION;
 
 const getS3Client = (
   region: string,
@@ -21,28 +26,28 @@ const getS3Client = (
   return s3Client;
 };
 
-// Get a presigned URL for a specific file in S3.
-const getS3ProductSignedUrl = async (
-  s3AccessKeyId: string,
-  s3SecretAccessKey: string,
-  bucket: string,
-  region: string,
-  filePath: string,
-  expiryMinutes: number,
-) => {
-  const s3Client = getS3Client(region, s3AccessKeyId, s3SecretAccessKey);
+// // Get a presigned URL for a specific file in S3.
+// const getS3ProductSignedUrl = async (
+//   s3AccessKeyId: string,
+//   s3SecretAccessKey: string,
+//   bucket: string,
+//   region: string,
+//   filePath: string,
+//   expiryMinutes: number,
+// ) => {
+//   const s3Client = getS3Client(region, s3AccessKeyId, s3SecretAccessKey);
 
-  const params = {
-    Bucket: bucket,
-    Key: filePath,
-    Expires: expiryMinutes * 60,
-  };
-  const command = new GetObjectCommand(params);
-  const digitalAssetUrl = await getSignedUrl(s3Client, command, {
-    expiresIn: 3600,
-  });
-  return digitalAssetUrl;
-};
+//   const params = {
+//     Bucket: bucket,
+//     Key: filePath,
+//     Expires: expiryMinutes * 60,
+//   };
+//   const command = new GetObjectCommand(params);
+//   const digitalAssetUrl = await getSignedUrl(s3Client, command, {
+//     expiresIn: 3600,
+//   });
+//   return digitalAssetUrl;
+// };
 
 const s3AddProduct = async (
   s3AccessKeyId: string,
@@ -75,6 +80,33 @@ const s3AddProduct = async (
   }
 };
 
+const s3AddProductWithAppCreds = async (
+  title: string,
+  file: Buffer | Uint8Array | Blob | string | ReadableStream<any>,
+  fileType: string,
+  fileName: string,
+) => {
+  const s3Client = getS3Client(S3_REGION, S3_ACCESS_KEY, S3_SECRET_ACCESS_KEY);
+  try {
+    // Write
+    const s3AddProductResponse = await s3Client.send(
+      new PutObjectCommand({
+        Bucket: S3_BUCKET,
+        Key: fileName,
+        Body: file,
+        ContentType: fileType,
+        Metadata: {
+          title: title,
+        },
+      }),
+    );
+    return { ETag: s3AddProductResponse.ETag, success: true };
+  } catch (err) {
+    console.error("S3 failed adding new product:", err);
+    return { success: false };
+  }
+};
+
 // Test user's permissions.
 const s3CredsTest = async (
   s3AccessKeyId: string,
@@ -85,7 +117,7 @@ const s3CredsTest = async (
   const s3Client = getS3Client(region, s3AccessKeyId, s3SecretAccessKey);
   const testKey = "digiful-test-file_delete-me.txt";
   const testContent =
-    "This file was created by dijiful in order to test your S3 credentioals. You can delete this file.";
+    "This file was created by digiful in order to test your S3 credentioals. You can delete this file.";
   try {
     // Write
     await s3Client.send(
@@ -118,4 +150,4 @@ const s3CredsTest = async (
   }
 };
 
-export { getS3ProductSignedUrl, s3CredsTest, s3AddProduct };
+export { s3CredsTest, s3AddProduct, s3AddProductWithAppCreds };
---
+++ app/utils/utilities.ts
@@ -0,0 +1,8 @@
+const userFriendlyDate = (date: string) =>
+  new Date(date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+export { userFriendlyDate };
