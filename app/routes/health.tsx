cab0d75edaac452722294564d00dfcdb182e9f2f
Steve <steve@yarrumevets.com>
2025-07-19 13:56:42 -0700

    Added health route to provide a means of health checks for the AWS app load balancer

---
+++ app/routes/health.tsx
@@ -0,0 +1,4 @@
+export const loader = () => new Response("OK", { status: 200 });
+
+// This route is being used for health checks performed by AWS ALB (application load balancer).
+// path: <domain>/health
