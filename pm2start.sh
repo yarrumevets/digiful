b441935a3bde0d0d4bbb3ee9f97fa7be069d1112
Steve <steve@yarrumevets.com>
2025-07-19 14:37:35 -0700

    Updated notes in the pm2start script and also rand the git script to track the permissions change in the pm2start.sh script.

--- pm2start.sh
+++ pm2start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-# Before first running:
+
+# Before first commit, this applies permissions that persist in git:
+# git update-index --chmod=+x yourscript.sh
+
+# In order to run with pm2 and env vars you need:
 # npm install -g dotenv-cli
-# chmod +x pm2start.sh
-# Run this with:
+
+# Copy and run this PM2 command:
 # pm2 start ./pm2start.sh --name digiful
+
 dotenv npm run start
