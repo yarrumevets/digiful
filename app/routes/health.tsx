69a9c6c3952e7d9aa4559d974eb772ca816ea80b
Steve <steve@yarrumevets.com>
2025-07-19 16:37:41 -0700

    Added health-check console log for debugging.

--- app/routes/health.tsx
+++ app/routes/health.tsx
@@ -1,4 +1,7 @@
-export const loader = () => new Response("OK", { status: 200 });
+export const loader = () => {
+  console.log("health check");
+  return new Response("OK", { status: 200 });
+};
 
 // This route is being used for health checks performed by AWS ALB (application load balancer).
 // path: <domain>/health
