e43a25af17b2719671c7a39d84057650ecc86e09
Steve <steve@yarrumevets.com>
2025-07-19 15:22:08 -0700

    reverted back to a script that requires manual permission mod

--- pm2start.sh
+++ pm2start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Before first commit:
-# git update-index --chmod=+x yourscript.sh
-
 # Before first running:
 # npm install -g dotenv-cli
+# chmod +x pm2start.sh
 
 # Run this with:
 # pm2 start ./pm2start.sh --name digiful
