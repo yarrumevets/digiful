4b088033da3ca2aef3163713919af718eb9522f0
Steve <steve@yarrumevets.com>
2025-07-22 15:28:40 -0700

    Added a health check route for Route53 failover.

--- app/routes/health.tsx
+++ app/routes/health.tsx
@@ -2,6 +2,3 @@ export const loader = () => {
   console.log("\u2764\uFE0F HEALTH CHECK \u2764\uFE0F");
   return new Response("OK", { status: 200 });
 };
-
-// This route is being used for health checks performed by AWS ALB (application load balancer).
-// path: <domain>/health
