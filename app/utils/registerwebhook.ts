5d34cb0f7ff5a2c732a048e655af01f1a29e737d
Steve <steve@yarrumevets.com>
2025-07-24 23:13:05 -0700

    Added a util function to register webhooks to clean up app page after adding another webhook registration.

---
+++ app/utils/registerwebhook.ts
@@ -0,0 +1,100 @@
+import type { Session } from "@shopify/shopify-app-remix/server";
+import type { ShopifyRestResources } from "node_modules/@shopify/shopify-api/dist/ts/rest/types";
+import type { AdminApiContextWithRest } from "node_modules/@shopify/shopify-app-remix/dist/ts/server/clients";
+import { mongoClientPromise } from "./mongoclient";
+import { resJson } from "./utilities";
+import { encrypt } from "app/utils/encrypt";
+
+const client = await mongoClientPromise;
+const db = client.db(process.env.DB_NAME);
+const MERCHANT_COLLECTION = "" + process.env.MERCHANT_COLLECTION;
+
+export const registerWebhook = async (
+  shopId: string,
+  admin: AdminApiContextWithRest<ShopifyRestResources>,
+  session: Session,
+  webhookRoute: string,
+  mongoSubsObjectName: string, // name of the object to store subs info in merchant.
+  topic: string, // APP_SUBSCRIPTIONS_UPDATE
+) => {
+  // Example data that is passed in:
+  const webhookUrl = "" + process.env.WEBHOOK_URL + webhookRoute;
+
+  const mongoData = await db
+    .collection(MERCHANT_COLLECTION)
+    .findOne({ shopId });
+  // Check for existing webhook
+  const listResp = await admin.graphql(
+    `query {
+            webhookSubscriptions(topics: [${JSON.stringify(topic)}], first: 10) # <--- need variable for APP_SUBSCRIPTIONS_UPDATE
+            {
+              edges {
+                node {
+                  id
+                  callbackUrl
+                }
+              }
+            }
+          }`,
+  );
+  const listData = await listResp.json();
+  const sub = listData.data.webhookSubscriptions.edges
+    .map((e: any) => e.node)
+    .find((n: any) => n.callbackUrl === webhookUrl);
+  if (sub?.id) {
+    return resJson({
+      success: true,
+      alreadyExisted: true,
+    });
+  }
+
+  if (!mongoData?.[mongoSubsObjectName]?.id && session.accessToken) {
+    const gqlResp = await admin.graphql(
+      `mutation {
+              webhookSubscriptionCreate(topic: ${JSON.stringify(topic)}, webhookSubscription: { # <--- and here
+                callbackUrl: "${webhookUrl}",
+                format: JSON
+              }) {
+                webhookSubscription { 
+                  id
+                  topic
+                  createdAt
+                }
+                userErrors { 
+                  field 
+                  message 
+                }
+              }
+            }`,
+    );
+    const { data } = await gqlResp.json();
+    if (data.webhookSubscriptionCreate.userErrors?.length) {
+      console.error(
+        "\u26A0\uFE0F  Webhook registration user errors: ",
+        data.webhookSubscriptionCreate.userErrors,
+      );
+      await db.collection(MERCHANT_COLLECTION).updateOne(
+        { shopId },
+        {
+          $set: {
+            [mongoSubsObjectName]: {
+              errors: data.webhookSubscriptionCreate.userErrors,
+            },
+          },
+        },
+      );
+    } else {
+      const webhookData = data.webhookSubscriptionCreate.webhookSubscription;
+      webhookData.accessToken = encrypt(session.accessToken);
+      await db.collection(MERCHANT_COLLECTION).updateOne(
+        { shopId },
+        {
+          $set: { [mongoSubsObjectName]: webhookData },
+        },
+      );
+    }
+  }
+  return resJson({
+    success: true,
+  });
+};
